d7ffd6003294254215e42d22ebc9cd2bbe161cc8
Jakub Generovic <55619673+aur0ra-dev@users.noreply.github.com>
2022-06-23 18:03:26 +0200

    Add files via upload

---
+++ Documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"application"},{"l":"application.human"},{"l":"application.human.fromHuman"},{"l":"application.places"},{"l":"application.simulation"}];updateSearchResults();
